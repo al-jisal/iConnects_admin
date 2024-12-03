7f0d87b856095ea8dfa6d585395ebfc3e05465bb
al_jisal <aljisal.frimpong@gmail.com>
2024-12-03 13:49:57 -0500

    generated filepaths to the data using pkg_resources

--- project/data.py
+++ project/data.py
@@ -1,4 +1,5 @@
 import json
+from pkg_resources import resource_filename
 from project.models import Student, Alum, Post
 
 def load_students_from_json(file_path):
@@ -15,8 +16,12 @@ def load_posts_from_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
     
+# file paths
+students_data_filepath = resource_filename('project', 'data/students.json')
+alumni_data_filepath = resource_filename('project', 'data/alumni.json')
+posts_data_filepath = resource_filename('project', 'data/posts.json')
 
 # Load data
-students_data = load_students_from_json("students.json")
-alumni_data = load_alumni_from_json("alumni.json")
-posts_data = load_posts_from_json("posts.json")
+students_data = load_students_from_json(students_data_filepath)
+alumni_data = load_alumni_from_json(alumni_data_filepath)
+posts_data = load_posts_from_json(posts_data_filepath)
